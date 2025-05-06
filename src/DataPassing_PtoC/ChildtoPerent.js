ae0c5b62d2c92f3fe116cd3eef0e732c8f4b6aa4
najmul-haque-talukder <talukderrobiulhasan@gmail.com>
2025-05-07 00:00:23 +0600

    version 1

---
+++ src/DataPassing_PtoC/ChildtoPerent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ChildtoPerent(props) {
+
+    let data = "I am From Child Component"
+    props.onChildData(data)
+
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
